02dfc4f32e6618d903ddbd1e50537e9239445860
xieguigang <xie.guigang@gmail.com>
2016-05-30 01:27:42 +0800

    export function code readable improvements

--- Cytoscape/DocumentFormat/Graph/Serialization/ExportToFile.vb
+++ Cytoscape/DocumentFormat/Graph/Serialization/ExportToFile.vb
@@ -2,10 +2,13 @@
 Imports Microsoft.VisualBasic.DocumentFormat.Csv.StorageProvider.Reflection.Reflector
 Imports Microsoft.VisualBasic.DataVisualization.Network.FileStream.Node
 Imports Microsoft.VisualBasic.DataVisualization.Network.FileStream.NetworkEdge
+Imports Microsoft.VisualBasic.DataVisualization.Network.FileStream.NameOf
 Imports Microsoft.VisualBasic.DataVisualization.Network.LDM.Abstract
 Imports Microsoft.VisualBasic.DocumentFormat.Csv.StorageProvider.ComponentModels
 Imports Microsoft.VisualBasic.DataVisualization.Network
 Imports Microsoft.VisualBasic
+Imports Microsoft.VisualBasic.Linq
+Imports System.Runtime.CompilerServices
 
 Namespace DocumentFormat.CytoscapeGraphView.Serialization
 
@@ -15,8 +18,8 @@ Namespace DocumentFormat.CytoscapeGraphView.Serialization
     ''' <remarks></remarks>
     Public Module ExportToFile
 
-        Public Function Export(Of Edge As INetworkEdge)(NodeList As IEnumerable(Of FileStream.Node), Edges As IEnumerable(Of Edge), Optional Title As String = "NULL") As Graph
-            Return Export(Of FileStream.Node, Edge)(NodeList.ToArray, Edges.ToArray, Title)
+        Public Function Export(Of Edge As INetworkEdge)(nodes As IEnumerable(Of FileStream.Node), edges As IEnumerable(Of Edge), Optional title As String = "NULL") As Graph
+            Return Export(Of FileStream.Node, Edge)(nodes.ToArray, edges.ToArray, title)
         End Function
 
         ''' <summary>
@@ -87,7 +90,7 @@ Namespace DocumentFormat.CytoscapeGraphView.Serialization
                                   NodeTypeMapping As Dictionary(Of String, Type),
                                   EdgeTypeMapping As Dictionary(Of String, Type),
                                   Optional Title As String = "NULL") As Graph
-            Dim Model As Graph = New Graph With {
+            Dim Model As New Graph With {
                 .Label = "0",
                 .ID = "1",
                 .Directed = "1"
@@ -106,9 +109,11 @@ Namespace DocumentFormat.CytoscapeGraphView.Serialization
             }
             Dim EdgeSchema = SchemaProvider.CreateObject(GetType(Edge), False)
             Dim interMaps = __mapInterface(EdgeSchema)
+            Dim hash As Dictionary(Of String, DocumentElements.Node) =
+                Model.Nodes.ToDictionary(Function(x) x.label)
 
             Model.Nodes = __exportNodes(NodeList, NodeTypeMapping)
-            Model.Edges = __exportEdges(Edges, Model.Nodes.ToDictionary(Function(item) item.label), EdgeTypeMapping, interMaps)
+            Model.Edges = __exportEdges(Edges, hash, EdgeTypeMapping, interMaps)
             Model.Attributes = ModelAttributes
 
             Return Model
@@ -121,16 +126,20 @@ Namespace DocumentFormat.CytoscapeGraphView.Serialization
             Dim mapNodes = schema.DeclaringType.GetInterfaceMap(GetType(I_InteractionModel))
             Dim maps As New Dictionary(Of String, String)
 
-            Dim edgeMaps = (From i As Integer In mapEdge.TargetMethods.Sequence
+            Dim edgeMaps = (From i As Integer
+                            In mapEdge.TargetMethods.Sequence
                             Let [interface] = mapEdge.InterfaceMethods(i)
                             Where InStr([interface].Name, propGET) = 1
                             Select [interface],
-                                mMethod = mapEdge.TargetMethods(i)).ToDictionary(Function(x) x.interface.Name.Replace(propGET, ""))
-            Dim nodeMaps = (From i As Integer In mapNodes.TargetMethods.Sequence
+                                mMethod = mapEdge.TargetMethods(i)) _
+                                .ToDictionary(Function(x) x.interface.Name.Replace(propGET, ""))
+            Dim nodeMaps = (From i As Integer
+                            In mapNodes.TargetMethods.Sequence
                             Let [interface] = mapNodes.InterfaceMethods(i)
                             Where InStr([interface].Name, propGET) = 1
                             Select [interface],
-                                mMethod = mapNodes.TargetMethods(i)).ToDictionary(Function(x) x.interface.Name.Replace(propGET, ""))
+                                mMethod = mapNodes.TargetMethods(i)) _
+                                .ToDictionary(Function(x) x.interface.Name.Replace(propGET, ""))
 
             Dim map = nodeMaps(NameOf(I_InteractionModel.locusId)) : Call maps.Add(REFLECTION_ID_MAPPING_FROM_NODE, __getMap(map.interface, map.mMethod, schema))
             map = nodeMaps(NameOf(I_InteractionModel.Address)) : Call maps.Add(REFLECTION_ID_MAPPING_TO_NODE, __getMap(map.interface, map.mMethod, schema))
@@ -140,9 +149,12 @@ Namespace DocumentFormat.CytoscapeGraphView.Serialization
             Return maps
         End Function
 
-        Private Function __getMap([interface] As Reflection.MethodInfo, mMethod As Reflection.MethodInfo, schema As SchemaProvider) As String
+        Private Function __getMap([interface] As Reflection.MethodInfo,
+                                  mMethod As Reflection.MethodInfo,
+                                  schema As SchemaProvider) As String
+
             Dim mapName As String = mMethod.Name.Replace(propGET, "")
-            Dim mapFiled = schema.GetField(mapName)
+            Dim mapFiled As StorageProvider = schema.GetField(mapName)
 
             If mapFiled Is Nothing Then
                 Return mapName
@@ -159,7 +171,7 @@ Namespace DocumentFormat.CytoscapeGraphView.Serialization
         ''' <remarks></remarks>
         Private Function __createTypeMapping(typeMapping As Dictionary(Of String, Type)) As Func(Of String, String)
             If typeMapping.IsNullOrEmpty Then
-                Return Function(NULL As String) DocumentElements.Attribute.ATTR_VALUE_TYPE_STRING
+                Return Function(null) ATTR_VALUE_TYPE_STRING
             End If
 
             Dim CytoscapeMapping As Dictionary(Of Type, String) = DocumentElements.Attribute.TypeMapping
@@ -170,28 +182,33 @@ Namespace DocumentFormat.CytoscapeGraphView.Serialization
         Private Function __mapping(attrKey As String,
                                    typeMapping As Dictionary(Of String, Type),
                                    cytoscapeMapping As Dictionary(Of Type, String)) As String
-            Dim Type As Type = typeMapping.TryGetValue(attrKey)
-            If Not Type Is Nothing AndAlso cytoscapeMapping.ContainsKey(Type) Then
-                Return cytoscapeMapping(Type)
+            Dim type As Type = typeMapping.TryGetValue(attrKey)
+            If Not type Is Nothing AndAlso cytoscapeMapping.ContainsKey(type) Then
+                Return cytoscapeMapping(type)
             Else
-                Return DocumentElements.Attribute.ATTR_VALUE_TYPE_STRING
+                Return ATTR_VALUE_TYPE_STRING
             End If
         End Function
 
-        Private Function __exportNodes(Of Node As INode)(Nodes As Node(), nodeTypeMapping As Dictionary(Of String, Type)) As DocumentElements.Node()
-            Dim ChunkBuffer = Nodes.ExportAsPropertyAttributes(False)
-            Dim typeMapping = __createTypeMapping(nodeTypeMapping)
-            Dim LQuery = (From item In ChunkBuffer.AsParallel
-                          Let node_obj = __exportNode(item, __getType:=typeMapping)
-                          Select node_obj
-                          Group node_obj By node_obj.label Into Group
-                          Order By label Ascending).ToArray
-            Return (From x In LQuery Select x.Group.First).ToArray.AddHandle.ToArray  '生成节点数据并去除重复
+        Private Function __exportNodes(Of Node As INode)(nodes As Node(), nodeTypeMapping As Dictionary(Of String, Type)) As DocumentElements.Node()
+            Dim buf As List(Of Dictionary(Of String, String)) =
+                nodes.ExportAsPropertyAttributes(False)
+            Dim typeMapping As Func(Of String, String) =
+                __createTypeMapping(nodeTypeMapping)
+            Dim LQuery = From x As Dictionary(Of String, String)
+                         In buf.AsParallel
+                         Let node_obj = __exportNode(x, __getType:=typeMapping)
+                         Select node_obj
+                         Group node_obj By node_obj.label Into Group
+                         Order By label Ascending
+            Return LQuery.Select(Function(x) x.Group) _
+                .ToArray(AddressOf DefaultFirst) _
+                .AddHandle.ToArray  '生成节点数据并去除重复
         End Function
 
         Private Function __exportNode(dict As Dictionary(Of String, String), __getType As Func(Of String, String)) As DocumentElements.Node
             Dim ID As String = dict(REFLECTION_ID_MAPPING_IDENTIFIER)
-            Dim attrs As List(Of DocumentElements.Attribute) = New List(Of DocumentElements.Attribute)
+            Dim attrs As New List(Of DocumentElements.Attribute)
 
             attrs += New DocumentElements.Attribute With {
                 .Name = ATTR_SHARED_NAME,
@@ -205,20 +222,20 @@ Namespace DocumentFormat.CytoscapeGraphView.Serialization
             }
             Call dict.Remove(REFLECTION_ID_MAPPING_IDENTIFIER)
 
-            attrs += (From item As KeyValuePair(Of String, String)
-                      In dict
-                      Select New DocumentElements.Attribute With {
-                          .Name = item.Key,
-                          .Value = item.Value,
-                          .Type = __getType(item.Key)}).ToArray
+            attrs += From item As KeyValuePair(Of String, String)
+                     In dict
+                     Select New DocumentElements.Attribute With {
+                         .Name = item.Key,
+                         .Value = item.Value,
+                         .Type = __getType(item.Key)
+                     }
 
-            Dim Node As DocumentElements.Node =
-                New DocumentElements.Node With {
-                    .label = ID,
-                    .Attributes = attrs.ToArray
+            Dim node As New DocumentElements.Node With {
+                .label = ID,
+                .Attributes = attrs.ToArray
             }
 
-            Return Node
+            Return node
         End Function
 
         Private Function __exportEdges(Of Edge As INetworkEdge)(
@@ -227,20 +244,21 @@ Namespace DocumentFormat.CytoscapeGraphView.Serialization
                                           EdgeTypeMapping As Dictionary(Of String, Type),
                                           Schema As Dictionary(Of String, String)) As DocumentElements.Edge()
 
-            Dim ChunkBuffer = __mapNodes(Edges.ExportAsPropertyAttributes(False), Schema)
-            Dim TypeMapping = __createTypeMapping(EdgeTypeMapping)
-            Dim LQuery = (From item As Dictionary(Of String, String)
-                          In ChunkBuffer
-                          Select __exportEdge(item, Nodes, TypeMapping)).ToArray.AddHandle(offset:=Nodes.Count)
+            Dim buf = __mapNodes(Edges.ExportAsPropertyAttributes(False), Schema)
+            Dim typeMapping As Func(Of String, String) =
+                __createTypeMapping(EdgeTypeMapping)
+            Dim LQuery As DocumentElements.Edge() =
+                buf.ToArray(Function(x) x.__exportEdge(Nodes, typeMapping)) _
+                .AddHandle(offset:=Nodes.Count)
             Return LQuery
         End Function
 
         Private Function __mapNodes(ByRef buffer As List(Of Dictionary(Of String, String)), Schema As Dictionary(Of String, String)) As List(Of Dictionary(Of String, String))
-            For Each dict In buffer
-                For Each map In Schema
+            For Each dict As Dictionary(Of String, String) In buffer
+                For Each map As KeyValuePair(Of String, String) In Schema
                     If Not dict.ContainsKey(map.Key) Then
                         If dict.ContainsKey(map.Value) Then
-                            Dim value = dict(map.Value)
+                            Dim value As String = dict(map.Value)
                             Call dict.Add(map.Key, value)
                         End If
                     End If
@@ -250,6 +268,7 @@ Namespace DocumentFormat.CytoscapeGraphView.Serialization
             Return buffer
         End Function
 
+        <Extension>
         Private Function __exportEdge(dict As Dictionary(Of String, String), Nodes As Dictionary(Of String, DocumentElements.Node), __getType As Func(Of String, String)) As DocumentElements.Edge
             Dim nodeName As String = dict(REFLECTION_ID_MAPPING_FROM_NODE)
             Dim fromNode As DocumentElements.Node = Nodes.TryGetValue(nodeName)
@@ -280,22 +299,17 @@ Namespace DocumentFormat.CytoscapeGraphView.Serialization
             Dim InteractionType As String = dict.TryGetValue(REFLECTION_ID_MAPPING_INTERACTION_TYPE)
             InteractionType = If(String.IsNullOrEmpty(InteractionType), "interact", InteractionType)
 
-            Dim Node As DocumentElements.Edge =
-                New DocumentElements.Edge With {
-                    .Label = String.Format("{0} ({1}) {2}", fromNode.label, InteractionType, toNode.label),
-                    .source = fromNode.id,
-                    .target = toNode.id
-            }
+            Dim label As String = String.Format("{0} ({1}) {2}", fromNode.label, InteractionType, toNode.label)
             Dim attrs As List(Of DocumentElements.Attribute) =
                 New List(Of DocumentElements.Attribute)
             attrs += New DocumentElements.Attribute With {
                 .Name = ATTR_SHARED_NAME,
-                .Value = Node.Label,
+                .Value = label,
                 .Type = ATTR_VALUE_TYPE_STRING
             }
             attrs += New DocumentElements.Attribute With {
                 .Name = ATTR_NAME,
-                .Value = Node.Label,
+                .Value = label,
                 .Type = ATTR_VALUE_TYPE_STRING
             }
             attrs += New DocumentElements.Attribute With {
@@ -306,12 +320,21 @@ Namespace DocumentFormat.CytoscapeGraphView.Serialization
             Call dict.Remove(REFLECTION_ID_MAPPING_FROM_NODE)
             Call dict.Remove(REFLECTION_ID_MAPPING_TO_NODE)
             Call dict.Remove(REFLECTION_ID_MAPPING_INTERACTION_TYPE)
-            Call attrs.AddRange((From item As KeyValuePair(Of String, String) In dict
-                                 Select New DocumentElements.Attribute With {
-                                     .Name = item.Key,
-                                     .Value = item.Value,
-                                     .Type = __getType(item.Key)}).ToArray)
-            Node.Attributes = attrs.ToArray
+
+            attrs += From item As KeyValuePair(Of String, String)
+                     In dict
+                     Select New DocumentElements.Attribute With {
+                         .Name = item.Key,
+                         .Value = item.Value,
+                         .Type = __getType(item.Key)
+                     }
+
+            Dim Node As New DocumentElements.Edge With {
+                    .Label = label,
+                    .source = fromNode.id,
+                    .target = toNode.id,
+                    .Attributes = attrs
+            }
             Return Node
         End Function
     End Module
