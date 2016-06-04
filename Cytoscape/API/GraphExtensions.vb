56e09461ddf2760596d18cd3cc76f811bad2ea5c
xieguigang <xie.guigang@gmail.com>
2016-06-05 05:47:02 +0800

    finish the convert api

--- Cytoscape/API/GraphExtensions.vb
+++ Cytoscape/API/GraphExtensions.vb
@@ -1,6 +1,10 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports LANS.SystemsBiology.AnalysisTools.DataVisualization.Interaction.Cytoscape.CytoscapeGraphView
 Imports LANS.SystemsBiology.AnalysisTools.DataVisualization.Interaction.Cytoscape.CytoscapeGraphView.XGMML
+Imports Microsoft.VisualBasic.DataVisualization
 Imports Microsoft.VisualBasic.DataVisualization.Network.Graph
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic
 
 Namespace API
 
@@ -8,7 +12,24 @@ Namespace API
 
         <Extension>
         Public Function CreateGraph(g As Graph) As NetworkGraph
+            Dim nodes As Network.Graph.Node() =
+                LinqAPI.Exec(Of Network.Graph.Node) <= From n As XGMML.Node
+                                                       In g.Nodes
+                                                       Select New Network.Graph.Node(n.label)
+            Dim edges As Network.Graph.Edge() =
+                LinqAPI.Exec(Of Network.Graph.Edge) <= From edge As XGMML.Edge
+                                                       In g.Edges
+                                                       Select New Network.Graph.Edge(
+                                                           CStr(edge.Id),
+                                                           nodes(edge.source),
+                                                           nodes(edge.target),
+                                                           New EdgeData)
+            Dim net As New NetworkGraph() With {
+                .nodes = New List(Of Network.Graph.Node)(nodes),
+                .edges = New List(Of Network.Graph.Edge)(edges)
+            }
 
+            Return net
         End Function
     End Module
 End Namespace
