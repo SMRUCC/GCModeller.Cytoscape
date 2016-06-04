35a3ff8b810513fc1d5ca1241c5a978ec3e88cab
xieguigang <xie.guigang@gmail.com>
2016-06-05 05:38:20 +0800

    try convert cytoscape graph to network graph model

---
+++ Cytoscape/API/GraphExtensions.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Runtime.CompilerServices
+Imports LANS.SystemsBiology.AnalysisTools.DataVisualization.Interaction.Cytoscape.CytoscapeGraphView.XGMML
+Imports Microsoft.VisualBasic.DataVisualization.Network.Graph
+
+Namespace API
+
+    Public Module GraphExtensions
+
+        <Extension>
+        Public Function CreateGraph(g As Graph) As NetworkGraph
+
+        End Function
+    End Module
+End Namespace
