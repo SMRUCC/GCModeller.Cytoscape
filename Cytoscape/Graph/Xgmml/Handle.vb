56e01284ab88b6b6d181316bd8eb36602e39c823
xieguigang <xie.guigang@gcmodeller.org>
2019-11-02 18:11:33 +0800

    parse handle string

--- Cytoscape/Graph/Xgmml/Handle.vb
+++ Cytoscape/Graph/Xgmml/Handle.vb
@@ -8,5 +8,45 @@
     ''' </remarks>
     Public Class Handle
 
+        Dim cosTheta# = Double.NaN
+        Dim sinTheta# = Double.NaN
+        Dim ratio# = Double.NaN
+
+        ' Original handle location
+        Dim x# = 0
+        Dim y# = 0
+
+        Const DELIMITER As Char = ","c
+
+        ''' <summary>
+        ''' Serialized string Is "cos,sin,ratio".
+        ''' </summary>
+        ''' <returns></returns>
+        Public Function getSerializableString() As String
+            Return cosTheta & DELIMITER & sinTheta & DELIMITER & ratio
+        End Function
+
+        Public Shared Function parseHandles(strRepresentation As String) As IEnumerable(Of Handle)
+            Return strRepresentation _
+                .Split("|"c) _
+                .Select(Function(str)
+                            Dim parts As Double() = str _
+                                .Split(DELIMITER) _
+                                .Select(AddressOf Double.Parse) _
+                                .ToArray
+
+                            If parts.Length = 2 Then
+                                Return New Handle With {.x = parts(0), .y = parts(1)}
+                            ElseIf parts.Length = 3 Then
+                                Return New Handle With {
+                                    .cosTheta = parts(0),
+                                    .sinTheta = parts(1),
+                                    .ratio = parts(3)
+                                }
+                            Else
+                                Return Nothing
+                            End If
+                        End Function)
+        End Function
     End Class
 End Namespace
